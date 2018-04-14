1df5b18bf9e99adb10299c2dccfce3dcec82d18b
sheerlinvuong <sheerlinvuong@gmail.com>
2018-04-14 18:32:47 +0100

    updated stream.js

--- stream.js
+++ stream.js
@@ -7,31 +7,43 @@ var myReadStream = fs.createReadStream(__dirname + '/readMe.txt', 'utf8');
 //myReadStream.pipe(myWriteStream);
 
 var base64Img = require('base64-img');
-var url = 'https://i0.wp.com/digital-photography-school.com/wp-content/uploads/2006/08/jpeg.png';
+var url = 'http://pluspng.com/img-png/face-hd-png-vladimir-putin-face-png-image-1645.png'
+base64Img.requestBase64(url, function(err, res, body){
+  console.log('image recieved');
+  //console.log(body);
+  img = body.split("data:image/png;base64,")[1];
 
-var img = base64Img.requestBase64(url, function(err, res, body) {
-    console.log('image recieved');
-    //console.log(body);
-    var b = body;
+  var length = img.length;
+  imageBytes = new ArrayBuffer(length);
+  console.log(imageBytes);
 
-    console.log(b[22,b.length]);
-});
+  var ua = new Uint8Array(imageBytes);
+  for (var i = 0; i < length; i++) {
+    ua[i] = img.charCodeAt(i);
 
-var AWS = require('aws-sdk');
+  }
+
+  //console.log(img);
+  var AWS = require('aws-sdk');
 AWS.config.loadFromPath('./config.json');
 var rekognition = new AWS.Rekognition({apiVersion: '2016-06-27'});
 
 var params = {
     Image: { 
-      Bytes: new Buffer('...') || 'STRING_VALUE' /* Strings will be Base-64 encoded on your behalf */,
-      /*S3Object: {
-        Bucket: 'STRING_VALUE',
-        Name: 'STRING_VALUE',
-        Version: 'STRING_VALUE'
-      } */
+      //Bytes: imageBytes,
+      S3Object: {
+        Bucket: 'node-sdk-sample-65511e90-57a2-4b5b-a4f8-148bc5c96dfa',
+        Name: 'putin.png',
+        //Version: 'STRING_VALUE'
+      } 
     }
   };
   rekognition.recognizeCelebrities(params, function(err, data) {
     if (err) console.log(err, err.stack); // an error occurred
     else     console.log(data);           // successful response
-  });
+  }); 
+
+
+});
+  
+
