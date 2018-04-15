0f9648b54a24ec29d192c3760038601f8b3588bd
sheerlinvuong <sheerlinvuong@gmail.com>
2018-04-15 20:10:45 +0100

    Minor

--- src/services/findceleb.js
+++ src/services/findceleb.js
@@ -1,8 +1,8 @@
 const AWS = require('aws-sdk');
 const path = require('path')
-AWS.config.loadFromPath(path.join(__dirname, 'config.json'))
+//AWS.config.loadFromPath(path.join(__dirname, 'config.json'))
 
-const rekognition = new AWS.Rekognition({ apiVersion: '2016-06-27' });
+const rekognition = new AWS.Rekognition({ apiVersion: '2016-06-27', region: 'eu-west-1' });
 
 module.exports = (buffer) => {
   return new Promise((res, rej) => {
