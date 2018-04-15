2c9a665481a76aa04b9ca0a682d0717a60e7879e
Harry Blum <himion0@gmail.com>
2018-04-15 20:15:12 +0100

    Chicken

--- src/services/findceleb.js
+++ src/services/findceleb.js
@@ -1,12 +1,12 @@
 const AWS = require('aws-sdk');
 const path = require('path')
-AWS.config.loadFromPath(path.join(__dirname, 'config.json'))
 
 const rekognition = new AWS.Rekognition({ apiVersion: '2016-06-27' });
 
 module.exports = (buffer) => {
   return new Promise((res, rej) => {
     rekognition.recognizeCelebrities({
+      
       Image: {
         Bytes: buffer,
       }
