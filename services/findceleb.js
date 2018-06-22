dec3ccea94d211b29648b97eb89a8cecf59b7224
Harry Blum <himion0@gmail.com>
2018-06-22 19:42:35 +0100

    simplified promise

--- services/findceleb.js
+++ services/findceleb.js
@@ -1,14 +1,5 @@
 const AWS = require("aws-sdk");
 const rekognition = new AWS.Rekognition({ region: "eu-west-1" });
 
-module.exports = buffer => {
-  return new Promise((res, rej) => {
-    rekognition.recognizeCelebrities({ Image: { Bytes: buffer } }, function(
-      err,
-      data
-    ) {
-      if (err) rej(err);
-      else res(data);
-    });
-  });
-};
+module.exports = buffer =>
+  rekognition.recognizeCelebrities({ Image: { Bytes: buffer } }).promise();
